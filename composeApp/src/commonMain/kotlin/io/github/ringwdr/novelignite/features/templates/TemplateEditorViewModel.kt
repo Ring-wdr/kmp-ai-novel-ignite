76fd4632e8d89e769790b11dc5ae326bca261983
ring-wdr <akswnd55@gmail.com>
2026-04-19 06:49:25 +0900

    fix: align template editor state shape

--- composeApp/src/commonMain/kotlin/io/github/ringwdr/novelignite/features/templates/TemplateEditorViewModel.kt
+++ composeApp/src/commonMain/kotlin/io/github/ringwdr/novelignite/features/templates/TemplateEditorViewModel.kt
@@ -1,14 +1,12 @@
 package io.github.ringwdr.novelignite.features.templates
 
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.update
 
 class TemplateEditorViewModel {
-    private val _state = MutableStateFlow(TemplateEditorState())
-    val state: StateFlow<TemplateEditorState> = _state
+    val state = MutableStateFlow(TemplateEditorState())
 
     fun addPromptBlock(value: String) {
-        _state.update { it.copy(promptBlocks = it.promptBlocks + value) }
+        state.update { it.copy(promptBlocks = it.promptBlocks + value) }
     }
 }
