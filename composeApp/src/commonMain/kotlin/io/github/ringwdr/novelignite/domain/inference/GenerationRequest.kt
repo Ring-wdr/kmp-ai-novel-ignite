f18f7ab632a9527921cc674194adc4cc964bd5f8
ring-wdr <akswnd55@gmail.com>
2026-04-19 06:09:11 +0900

    Require Task 6 generation template id

--- composeApp/src/commonMain/kotlin/io/github/ringwdr/novelignite/domain/inference/GenerationRequest.kt
+++ composeApp/src/commonMain/kotlin/io/github/ringwdr/novelignite/domain/inference/GenerationRequest.kt
@@ -2,7 +2,7 @@ package io.github.ringwdr.novelignite.domain.inference
 
 data class GenerationRequest(
     val projectId: String,
-    val templateId: String?,
+    val templateId: String,
     val actionType: String,
     val manuscriptExcerpt: String,
     val promptBlocks: List<String>,
